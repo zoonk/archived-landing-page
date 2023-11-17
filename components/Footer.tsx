e990f9bf336ffe5ecb283a12d1acc2a9328fe337
Will Ceolin <wceolin@users.noreply.github.com>
2023-11-16 23:06:10 -0300

    Footer: Update social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,12 +14,12 @@ import { getDictionary } from "@/dictionaries";
 import Link from "next/link";
 
 const socialLinks = [
-  { link: "https://github.com/zoonk", icon: IconBrandGithub },
-  { link: "https://x.com/zoonkeducation", icon: IconBrandX },
-  { link: "https://linkedin.com/company/zoonk", icon: IconBrandLinkedin },
-  { link: "https://instagram.com/zoonkeducation", icon: IconBrandInstagram },
-  { link: "https://threads.net/zoonkeducation", icon: IconBrandThreads },
-  { link: "https://facebook.com/zoonkeducation", icon: IconBrandFacebook },
+  { key: "github", icon: IconBrandGithub },
+  { key: "x", icon: IconBrandX },
+  { key: "linkedin", icon: IconBrandLinkedin },
+  { key: "instagram", icon: IconBrandInstagram },
+  { key: "threads", icon: IconBrandThreads },
+  { key: "facebook", icon: IconBrandFacebook },
 ];
 
 interface FooterProps {
@@ -32,10 +32,10 @@ export default async function Footer({ locale }: FooterProps) {
   return (
     <footer className="flex flex-col items-center border-t border-slate-400/10 mt-20 py-10 sm:flex-row-reverse sm:justify-between">
       <div className="flex gap-x-6">
-        {socialLinks.map(({ link, icon: Icon }) => (
+        {socialLinks.map(({ key, icon: Icon }) => (
           <a
-            key={link}
-            href={link}
+            key={key}
+            href={t.social[key]}
             className="text-slate-500 hover:text-slate-600 transition-colors"
           >
             <Icon size={24} />
