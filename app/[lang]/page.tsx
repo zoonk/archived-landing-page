7b6f548b46870d4578f8c082bd872d7014ca13eb
Will Ceolin <wceolin@users.noreply.github.com>
2024-05-28 15:57:11 -0300

    Home: Add wailist form

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,6 +3,11 @@ import { getDictionary } from "@/dictionaries";
 import type { LanguageParams } from "@/types";
 import type { Metadata } from "next";
 
+const WAILIST_FORM_URL = {
+  en: "https://docs.google.com/forms/d/e/1FAIpQLSe7ScgpDQMapGPoW97Wn0SnxClORESnN4umcjhipRwNdSBvww/viewform?embedded=true",
+  pt: "https://docs.google.com/forms/d/e/1FAIpQLScvl38CfvgMD59OO2rd6LezhEqmuvHQdsqLbqSDXyBh9BMNxg/viewform?embedded=true",
+};
+
 interface HomeProps {
   params: LanguageParams;
 }
@@ -31,6 +36,12 @@ export default async function Home({ params }: HomeProps) {
   return (
     <article>
       <Hero locale={params.lang} />
+
+      <div className="waitlist h-[1850px] md:h-[1644px]">
+        <iframe src={WAILIST_FORM_URL[params.lang]} width="640" height="1644">
+          Loading…
+        </iframe>
+      </div>
     </article>
   );
 }
