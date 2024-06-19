a15d510163fc45c2b9a9dfc3cd878e322908646e
Will Ceolin <wceolin@users.noreply.github.com>
2024-06-19 14:10:55 -0300

    remove unused features

--- app/[lang]/products/zoonk/page.tsx
+++ app/[lang]/products/zoonk/page.tsx
@@ -3,45 +3,6 @@ import { LanguageParams } from "@/types";
 import { Metadata } from "next";
 import { IconBrain } from "@tabler/icons-react";
 
-const features = [
-  {
-    name: "Push to deploy.",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.",
-    icon: null,
-  },
-  {
-    name: "SSL certificates.",
-    description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
-    icon: null,
-  },
-  {
-    name: "Simple queues.",
-    description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.",
-    icon: null,
-  },
-  {
-    name: "Advanced security.",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.",
-    icon: null,
-  },
-  {
-    name: "Powerful API.",
-    description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
-    icon: null,
-  },
-  {
-    name: "Database backups.",
-    description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ",
-    icon: null,
-  },
-];
-
 interface ProductProps {
   params: LanguageParams;
 }
